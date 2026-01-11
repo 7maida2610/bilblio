c0226a0fd28777086e8112f136d14660b8b2e8b9
7maida2610 <khlifahmed9@gmail.com>
2026-01-11 09:00:06 +0100

    Fix Version20251203231717 for PostgreSQL - escape user table name

--- migrations/Version20251203231717.php
+++ migrations/Version20251203231717.php
@@ -19,13 +19,29 @@ final class Version20251203231717 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, ADD phone VARCHAR(255) DEFAULT NULL, ADD billing_address JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', ADD shipping_address JSON DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+        
+        if ($isPostgres) {
+            // PostgreSQL syntax - use "user" (quoted) because user is a reserved word
+            $this->addSql('ALTER TABLE "user" ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, ADD phone VARCHAR(255) DEFAULT NULL, ADD billing_address JSON DEFAULT NULL, ADD shipping_address JSON DEFAULT NULL');
+        } else {
+            // MySQL syntax
+            $this->addSql('ALTER TABLE user ADD first_name VARCHAR(255) DEFAULT NULL, ADD last_name VARCHAR(255) DEFAULT NULL, ADD phone VARCHAR(255) DEFAULT NULL, ADD billing_address JSON DEFAULT NULL COMMENT \'(DC2Type:json)\', ADD shipping_address JSON DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        }
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP first_name, DROP last_name, DROP phone, DROP billing_address, DROP shipping_address');
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+        
+        if ($isPostgres) {
+            // PostgreSQL syntax
+            $this->addSql('ALTER TABLE "user" DROP first_name, DROP last_name, DROP phone, DROP billing_address, DROP shipping_address');
+        } else {
+            // MySQL syntax
+            $this->addSql('ALTER TABLE user DROP first_name, DROP last_name, DROP phone, DROP billing_address, DROP shipping_address');
+        }
     }
 }
