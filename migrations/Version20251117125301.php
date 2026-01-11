fe3602fc0d1e4d14b9e87b263b9a398575400207
7maida2610 <khlifahmed9@gmail.com>
2026-01-11 08:59:20 +0100

    Fix Version20251117125301 for PostgreSQL - escape user table name and use BOOLEAN instead of TINYINT

--- migrations/Version20251117125301.php
+++ migrations/Version20251117125301.php
@@ -19,13 +19,29 @@ final class Version20251117125301 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL, ADD verification_token VARCHAR(255) DEFAULT NULL');
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+        
+        if ($isPostgres) {
+            // PostgreSQL syntax - use "user" (quoted) because user is a reserved word
+            $this->addSql('ALTER TABLE "user" ADD is_verified BOOLEAN NOT NULL, ADD verification_token VARCHAR(255) DEFAULT NULL');
+        } else {
+            // MySQL syntax
+            $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL, ADD verification_token VARCHAR(255) DEFAULT NULL');
+        }
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP is_verified, DROP verification_token');
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+        
+        if ($isPostgres) {
+            // PostgreSQL syntax
+            $this->addSql('ALTER TABLE "user" DROP is_verified, DROP verification_token');
+        } else {
+            // MySQL syntax
+            $this->addSql('ALTER TABLE user DROP is_verified, DROP verification_token');
+        }
     }
 }
